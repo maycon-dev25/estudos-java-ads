8c14b25e1611d3c064de290c27fd109c4f62db53
Maycon <mayconvenancio7@gmail.com>
2026-03-27 21:24:50 -0300

    atualizar exemplo para demonstrar estrutura do-while

--- src/While.java
+++ src/While.java
@@ -1,4 +1,4 @@
-// Classe responsável por demonstrar o uso da estrutura de repetição while
+// Classe responsável por demonstrar o uso das estruturas de repetição while e do-while
 public class While {
 
     // Método principal - ponto inicial do programa
@@ -7,14 +7,28 @@ public class While {
         // Variável que controla o número de repetições
         int contador = 0;
 
-        // Enquanto o contador for menor ou igual a 5, o bloco será executado
-        while (contador <= 5) {
+//        // Estrutura while:
+//        // Executa o bloco enquanto a condição for verdadeira
+//        while (contador <= 5) {
+//
+//            // Exibe o valor atual do contador
+//            System.out.println("Contador (while): " + contador);
+//
+//            // Incrementa o contador em 1 a cada repetição
+//            contador++;
+//        }
+
+        // Estrutura do-while:
+        // Executa o bloco pelo menos uma vez, mesmo que a condição seja falsa
+        do {
 
             // Exibe o valor atual do contador
-            System.out.println("Contador: " + contador);
+            System.out.println("Contador (do-while): " + contador);
 
             // Incrementa o contador em 1 a cada repetição
             contador++;
-        }
+
+            // Continua repetindo enquanto o contador for menor ou igual a 5
+        } while (contador <= 5);
     }
 }
